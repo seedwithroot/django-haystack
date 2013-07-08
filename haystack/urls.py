1f3a91f76f801aeae451bc9bd120cd63f190c39a
Justin Lane <seedwithroot@datachamps-web1.(none)>
2013-07-08 20:14:52 +0000

    been a while

--- haystack/urls.py
+++ haystack/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls.defaults import *
-from haystack.views import SearchView
+from haystack.forms import FacetedSearchForm
+from haystack.query import SearchQuerySet
+from haystack.views import FacetedSearchView
+from decimal import *
 
+sqs = SearchQuerySet().facet('tags').facet('domain_authority').facet('page_rank')
+sqs_advanced = SearchQuerySet().all() #.facet('tags').facet('domain_authority').facet('page_rank')
+sqs_advanced.filter(domain_authority__in=['%.2f' % (x/100.00) for x in range(80, 100)]) #=Decimal('80')) #, prices__lt=Decimal('20.00'))
+#sqs_advanced.filter(domain_authority__gt=Decimal('80')) #, prices__lt=Decimal('20.00'))
+sqs_advanced.order_by('-domain_authority')
 
-urlpatterns = patterns('haystack.views',
-    url(r'^$', SearchView(), name='haystack_search'),
-)
+urlpatterns = patterns('haystack.views', 
+        url(r'^advanced/$', FacetedSearchView(form_class=FacetedSearchForm, searchqueryset=sqs_advanced), name='haystack_search_advanced'),
+        #url(r'^$', FacetedSearchView(form_class=FacetedSearchForm, searchqueryset=sqs), name='haystack_search'),
+
+        )
